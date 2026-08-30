78feba13fabcdf1137ebe498c21011740bd5ffc8
Haseeb-eng-56 <hs.haseebshahid2006@gmail.com>
2026-08-30 17:47:04 +0500

    feat(simulation): define circuit graph types and error codes (Phase A2)

---
+++ frontend/src/components/simulation/engine/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Simulation Engine - Public API
+ * Person 1: Simulation Engine
+ * All exports from engine modules
+ */
+
+export * from './units';
+export * from './errors';
+export * from './circuitGraph';
+
+// Future exports will be added as phases complete
+// Phase A3: circuitGraphBuilder
+// Phase A4: circuitValidator
+// Phase A5: dcSolver, circuitSolver
+// Phase A6: component models
+// Phase A7: measurements
+// Phase A8: graphData
